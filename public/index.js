10aa535f7f860d778fb86d121dfa9ba43ae7fd60
Helen <helenlin@princeton.edu>
2025-04-14 15:19:10 -0400

    avatar follow location

--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { FirstPersonControls } from "./FirstPersonControls.js";
 
 // create variables and make them available globally
 let scene, myRenderer, camera;
-
+let textureLoader;
 // keep track of which frame we are on
 let frameCount = 0;
 
@@ -19,30 +20,58 @@ function setupMySocket(){
   socket.on('msg', updateLocation);
 }
 
+function addModels() {
+  let modelLoader = new GLTFLoader();
+  let url = "assets/sm/bears.glb";
+
+  modelLoader.load(url, placeBear);
+}
+
+function placeBear(gltf) {
+  let mesh = gltf.scene;
+  //mesh.position.set(2, 0, 2);
+  //mesh.scale.set(5, 5, 5);
+  
+  // create our texture and load in an image file
+  let baseColor = new THREE.Color("rgb(0,0,0)");
+  
+  // create a normal texture
+  let normColor = textureLoader.load("assets/mat/Watermarked_normal.png");
+  
+  mesh.material = new THREE.MeshPhongMaterial( {map: baseColor, normalMap: normColor} );
+  
+  scene.add(mesh);
+
+}
+
 function createNewMesh(msg){
+
+  //addModels();
   let geo = new THREE.SphereGeometry(0.25, 20, 20);
   let mat = new THREE.MeshNormalMaterial();
-  let newMesh = new THREE.Mesh(geo,mat);
+  let newMesh = new THREE.Mesh(geo, mat);
 
   viewers.push(
     {id: msg.id,
     mesh: newMesh});
 
-  scene.add(newMesh);
   newMesh.position.set(msg.x,msg.y,msg.z);
+  scene.add(newMesh);
 }
 
 function updateLocation(msg){
   console.log(msg);
   let myMesh;
   for(let i = 0; i < viewers.length; i++){
-    if (viewers[i].id == msg.id) 
+    if (viewers[i].id == msg.id) { 
       myMesh = viewers[i].mesh;
+      myMesh.position.set(msg.x,msg.y,msg.z);
+    }
   }
   if (myMesh == null) {
     createNewMesh(msg);
   }
-  myMesh.position.set(msg.x,msg.y,msg.z);
+  
 }
 
 function onKeyDown(ev){
@@ -51,6 +80,7 @@ function onKeyDown(ev){
   (ev.key === "s") ||
   (ev.key === "d"))
     {
+      /*
     let myMessage = {
       id: socket.id,
       x: camera.position.x,
@@ -58,10 +88,12 @@ function onKeyDown(ev){
       z: camera.position.z
     };
     socket.emit('msg', myMessage);
+    */
   }
 }
 
 function init() {
+
 
   // create a scene and give it a background color
   scene = new THREE.Scene();
@@ -81,7 +113,6 @@ function init() {
   );
   camera.position.set(-6, 0, 0);
   camera.lookAt(6, 0, 0);
-
 
   // add orbit controls so we can navigate our scene while testing
   controls = new FirstPersonControls(scene, camera, myRenderer);
@@ -114,6 +145,7 @@ function init() {
   let directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   scene.add(directionalLight);
 
+
   window.addEventListener('keydown', onKeyDown);
 
   // start the draw loop
@@ -124,6 +156,15 @@ function draw() {
   controls.update();
   frameCount = frameCount + 1;
 
+
+  
+  let myMessage = {
+    id: socket.id,
+    x: camera.position.x,
+    y: camera.position.y,
+    z: camera.position.z
+  };
+  socket.emit('msg', myMessage);
 
 
   myRenderer.render(scene, camera);
